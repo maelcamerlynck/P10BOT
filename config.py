6564e21cc0f406ff5ef85fd1048aae35f2c4865b
Maël Camerlynck <mael.camerlynck@gmail.com>
2021-11-06 20:32:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ class DefaultConfig:
     LUIS_APP_ID = os.environ.get("LuisAppId", "b18d0106-84f2-409d-9ab8-34931a946bf0")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "bc9f746c79564223889a7f7b5344ddcd")
     # LUIS endpoint host name, ie "westeurope.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "westeurope.api.cognitive.microsoft.com")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "northeurope.api.cognitive.microsoft.com")
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get("InstrumentationKey", 'c5b691e9-1311-4755-a74a-55c3eb7d2a18')
     
