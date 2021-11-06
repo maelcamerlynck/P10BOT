8767a0daa49b9d540f49506f5f5a24364ccc62e0
Maël Camerlynck <mael.camerlynck@gmail.com>
2021-11-06 21:11:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "mael@camerlynck.fr")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "78xedpuP$")
+    APP_ID = os.environ.get("MicrosoftAppId", "784cc329-cc2e-4e09-8a47-2bfcf783769e")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "H{4gpX0K-3S9tf(j^FnG6]o*7>&")
     LUIS_APP_ID = os.environ.get("LuisAppId", "b18d0106-84f2-409d-9ab8-34931a946bf0")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "bc9f746c79564223889a7f7b5344ddcd")
     # LUIS endpoint host name, ie "westeurope.api.cognitive.microsoft.com"
