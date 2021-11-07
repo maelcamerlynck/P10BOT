2bb6c7becf598e04664cd95e950b042f1f84bea2
Maël Camerlynck <mael.camerlynck@gmail.com>
2021-11-07 20:52:59 +0100

    Update 02_cle_test.py

--- tests/02_cle_test.py
+++ tests/02_cle_test.py
@@ -2,9 +2,8 @@ from config import DefaultConfig
 
 def test_cle_insight():
     
-    assert DefaultConfig.APPINSIGHTS_INSTRUMENTATION_KEY == 'e1695f15-df98-4bc0-918a-75dfb8c1aa79'
+    assert DefaultConfig.APPINSIGHTS_INSTRUMENTATION_KEY == 'c5b691e9-1311-4755-a74a-55c3eb7d2a18'
     
 def test_cle_luis():
     
-    assert DefaultConfig.LUIS_API_KEY == '7bccf58a81bf4e43bedb92e2272fe87f'
-    
+    assert DefaultConfig.LUIS_API_KEY == 'bc9f746c79564223889a7f7b5344ddcd'
